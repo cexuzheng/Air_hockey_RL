8f7971495902fbea70b816ac6a6dd16cd8a7569e
cexuzheng <64259990+cexuzheng@users.noreply.github.com>
2022-10-11 04:26:34 +0200

    bugs_fixed

--- Generate_video.py
+++ Generate_video.py
@@ -1,4 +1,57 @@
 import numpy as np
-from game_engyne import ice_hockey
+from game_engyne import air_hockey
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+
+def update_line(i, lin_ball, ball_contours, lin_self_hand, self_hand_contours, lin_enemy_hand, enemy_hand_contours):
+    lin_ball.set_data( [ball_contours[i,:,0], ball_contours[i,:,1]] )
+    lin_self_hand.set_data( [self_hand_contours[i,:,0], self_hand_contours[i,:,1]] )
+    lin_enemy_hand.set_data( [enemy_hand_contours[i,:,0], enemy_hand_contours[i,:,1]] )
+    return lin_ball
+
+game = air_hockey(drag_coeff = 0.0, bounce_coeff=1.0);
+
+self = game
+game.enemy_hand_contour -= game.enemy_hand_pos
+game.enemy_hand_pos = np.array([0.5, 3.5])
+game.enemy_hand_contour += game.enemy_hand_pos
+
+game._draw()
+theta = np.random.rand()*np.pi
+game.set_ball_pos_vel([game.x_width/2, game.y_height/2],  np.random.rand()*6*np.array( np.cos(theta), np.sin(theta) ) )
+
+lin_ball = game.ax.plot( game.ball_contour[:,0], game.ball_contour[:,1], '--r' )[0]
+lin_self_hand = game.ax.plot( game.self_hand_contour[:,0], game.self_hand_contour[:,1], '--b' )[0]
+lin_enemy_hand = game.ax.plot( game.enemy_hand_contour[:,0], game.enemy_hand_contour[:,1], '--b' )[0]
+
+T = 9; n_loop = int(T/game.dt)
+ball_contour_shape = game.ball_contour.shape
+ball_contours = np.zeros( (n_loop, ball_contour_shape[0], ball_contour_shape[1]) )
+self_hand_contour_shape = game.self_hand_contour.shape
+self_hand_contours = np.zeros( (n_loop, self_hand_contour_shape[0], self_hand_contour_shape[1]) )
+enemy_hand_contour_shape = game.enemy_hand_contour.shape
+enemy_hand_contours = np.zeros( (n_loop, enemy_hand_contour_shape[0], enemy_hand_contour_shape[1]) )
+
+for i in range(n_loop):
+    print(i)
+    print(game.ball_pos)
+    game.compute_physics(True)
+    ball_contours[i,:,:] = game.ball_contour
+    self_hand_contours[i,:,:] = game.self_hand_contour
+    enemy_hand_contours[i,:,:] = game.enemy_hand_contour
+    
+# print( len( game.ball_walls_segments)  )
+
+print(game.self_hand_pos)
+print(game.enemy_hand_pos)
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=10, metadata=dict(artist='Me'), bitrate=1800)
+line_ani = animation.FuncAnimation( game.fig, update_line, n_loop,
+     fargs=(lin_ball, ball_contours, lin_self_hand, self_hand_contours, lin_enemy_hand, enemy_hand_contours), interval=150,save_count = 1000)
+
+
+line_ani.save('random.mp4', writer=writer)
+
+
+plt.show()
--- game_engyne.py
+++ game_engyne.py
@@ -15,8 +15,8 @@ GOAL_TO_ENEMY = 1; GOAL_TO_SELF = 2; WALL_COLLISION = 3; SELF_HAND_BALL_COLLISIO
 SELF_HAND_WALL_COLLISION = 6; ENEMY_HAND_WALL_COLLISION = 7
 
 class air_hockey:
-    def __init__(self, x_width = 4, y_height = 6, dt = 0.1, ball_radious = 0.1, hand_radious = 0.3,
-            goal_fraction = 0.5, m_ball = 0.1, m_hand = 20, drag_coeff = 0.1, bounce_coeff = 0.96):
+    def __init__(self, x_width = 4, y_height = 6, dt = 0.1, ball_radious = 0.1, hand_radious = 0.15,
+            goal_fraction = 0.5, m_ball = 0.1, m_hand = 0.2, drag_coeff = 0.1, bounce_coeff = 0.96):
         self.x_width = x_width;
         self.y_height = y_height;
         self.dt = dt;
@@ -25,26 +25,24 @@ class air_hockey:
         self.ball_radious = ball_radious;
         self.ball_pos = np.array(  [ ball_radious + np.random.rand()*( x_width - 2*ball_radious ), 
                     ball_radious + np.random.rand()*( y_height - 2*ball_radious ) ]  );         # [x,y]
-        self.ball_contour = np.array( [  np.cos(points), np.sin(points) ] ).T + self.ball_pos
+        self.ball_contour = ball_radious*np.array( [  np.cos(points), np.sin(points) ] ).T + self.ball_pos
         ball_v_angle = np.random.rand() * 2*np.pi 
         self.ball_vel = np.random.rand()*np.array([ np.cos(ball_v_angle), np.sin(ball_v_angle) ])
 
         self.hand_radious = hand_radious;
 
-        self.self_hand_pos = np.array(  [ hand_radious + np.random.rand()*( x_width - 2*hand_radious ), 
-                    hand_radious + np.random.rand()*( y_height - 2*hand_radious ) ]  );         # [x,y]
-        self.self_hand_contour = np.array( [  np.cos(points), np.sin(points) ] ).T + self.self_hand_pos
+        self.self_hand_pos = np.array(  [ x_width/2, hand_radious + y_height/2*0.3 ]  );         # [x,y]
+        self.self_hand_contour = hand_radious*np.array( [  np.cos(points), np.sin(points) ] ).T + self.self_hand_pos
         self.self_hand_vel = np.zeros(2)
 
-        self.enemy_hand_pos = np.array(  [ hand_radious + np.random.rand()*( x_width - 2*hand_radious ), 
-                    hand_radious + np.random.rand()*( y_height - 2*hand_radious ) ]  );         # [x,y]
-        self.enemy_hand_contour = np.array( [  np.cos(points), np.sin(points) ] ).T + self.enemy_hand_pos
+        self.enemy_hand_pos = np.array(  [ x_width/2, y_height*0.85 - hand_radious ]  );         # [x,y]
+        self.enemy_hand_contour = hand_radious*np.array( [  np.cos(points), np.sin(points) ] ).T + self.enemy_hand_pos
         self.enemy_hand_vel = np.zeros(2)
 
         goal_corner = (1-goal_fraction)/2
         self.goal_corner = goal_corner
         self.self_goal = [  [self.x_width*goal_corner, ball_radious], [self.x_width*(1-goal_corner), ball_radious]  ]
-        self.enemy_goal =[  [self.x_width*goal_corner, self.y_height - ball_radious], [self.x_width*(goal_corner), self.y_height - ball_radious]  ]
+        self.enemy_goal =[  [self.x_width*goal_corner, self.y_height - ball_radious], [self.x_width*(1-goal_corner), self.y_height - ball_radious]  ]
 
         self.m_ball = m_ball
         self.m_hand = m_hand
@@ -76,6 +74,11 @@ class air_hockey:
         hand_limits = hand_limits + [[  [hand_radious, self.y_height-hand_radious], [hand_radious, self.y_height/2+hand_radious]     ]]
         self.enemy_hand_walls_segments = hand_limits
 
+    def set_ball_pos_vel(self, ball_pos, ball_vel):
+        self.ball_contour -= self.ball_pos
+        self.ball_pos = np.array(ball_pos); self.ball_vel = np.array(ball_vel)
+        self.ball_contour += self.ball_pos
+
     def _draw(self):
         x_l2r = np.linspace(0, self.x_width, 11);
         y_d2u = np.linspace(0, self.y_height, 11);
@@ -88,25 +91,24 @@ class air_hockey:
 
         x_goal = np.linspace(self.x_width*self.goal_corner, self.x_width*(1-self.goal_corner), 11);
 
-        fig = plt.figure(figsize=(6, 9))
-        ax = fig.add_subplot(1,1,1) # ,projection="3d")
+        self.fig = plt.figure(figsize=(6, 9))
+        self.ax = self.fig.add_subplot(1,1,1) # ,projection="3d")
 
-        ax.plot(x_l2r,  y_down, 'b', linewidth = 4.0)
-        ax.plot(x_l2r,   y_up, 'b', linewidth = 4.0)
-        ax.plot(x_left,  y_d2u, 'b', linewidth = 4.0)
-        ax.plot(x_right, y_d2u, 'b', linewidth = 4.0)
+        self.ax.plot(x_l2r,  y_down, 'b', linewidth = 4.0)
+        self.ax.plot(x_l2r,   y_up, 'b', linewidth = 4.0)
+        self.ax.plot(x_left,  y_d2u, 'b', linewidth = 4.0)
+        self.ax.plot(x_right, y_d2u, 'b', linewidth = 4.0)
 
-        ax.plot(x_l2r, y_middle, c = soft_pink, linewidth = 2.0)
-        ax.plot(x_goal, y_down, c = 'k', linewidth = 6.0)
-        ax.plot(x_goal, y_up, c = 'k', linewidth = 6.0)
-        # ax.fill_between(x_l2r, 0,1, facecolor='red')
+        self.ax.plot(x_l2r, y_middle, c = soft_pink, linewidth = 2.0)
+        self.ax.plot(x_goal, y_down, c = 'k', linewidth = 6.0)
+        self.ax.plot(x_goal, y_up, c = 'k', linewidth = 6.0)
 
     def array_to_vel(self, trajectory):
         trajectory = np.array(trajectory)
         return( trajectory[1,:]-trajectory[0,:] )
 
     def vec_perp( self, vec ):
-        v_perp = [vec[1], -vec[0]]/np.linalg.norm(vec)
+        v_perp = [vec[1], -vec[0]]/np.max( [np.linalg.norm(vec), 1e-10] )
         return v_perp
 
     def check_wall_collision( self, trajectory, walls ):
@@ -122,10 +124,13 @@ class air_hockey:
         v2_dot_v1_perp[ np.abs(v2_dot_v1_perp) < 1e-10 ] = 1e-10       # avoid zero division
         lambdas_2 = -triangle_height/v2_dot_v1_perp
         x_collision = walls[:,0,:] + lambdas_2.reshape(n_walls, 1) * v2
-        lambdas = np.linalg.norm( x_collision - trajectory[1,:], axis= 1 ) / np.linalg.norm(v1)
-        bool_collision = np.logical_and( lambdas_2 > -1e-10, lambdas_2 < 1.0+1e-10)
-        bool_collision = np.logical_and( bool_collision, lambdas > -1e-10)
-        bool_collision = np.logical_and( bool_collision, lambdas < 1.0+1e-10)
+        bool_negative = np.linalg.norm( x_collision - trajectory[0,:] - v1, axis= 1 ) > np.linalg.norm( x_collision - trajectory[0,:] + v1, axis= 1 )
+        sg_lambda = np.ones(n_walls)
+        sg_lambda[bool_negative] = -1
+        lambdas = sg_lambda*np.linalg.norm( x_collision - trajectory[0,:], axis= 1 ) / np.max( [np.linalg.norm(v1), 1e-16])
+        bool_collision = np.logical_and( lambdas_2 > 1e-10, lambdas_2 < 1.0-1e-10)
+        bool_collision = np.logical_and( bool_collision, lambdas > 1e-10)
+        bool_collision = np.logical_and( bool_collision, lambdas < 1.0-1e-10) 
         return ( bool_collision, lambdas )
     
     def check_hand_collision(self, ball_trajectory, hand_trajectory):
@@ -177,26 +182,27 @@ class air_hockey:
         (self_hand_wall, l_self_hand_wall) = self.check_wall_collision( self_hand_trajectory, self.self_hand_walls_segments )
         (enemy_hand_wall, l_enemy_hand_wall) = self.check_wall_collision( enemy_hand_trajectory, self.enemy_hand_walls_segments )
         (wall_coll, l_wall_coll) = self.check_wall_collision( ball_trajectory, [self.self_goal, self.enemy_goal] + self.ball_walls_segments )
-        total_bool = np.concatenate( (self_hand, enemy_hand, self_hand_wall, enemy_hand_wall, wall_coll) )
+        total_bool = np.concatenate( ([self_hand], [enemy_hand], self_hand_wall, enemy_hand_wall, wall_coll) )
         # any_coll = np.any(total_bool); coll_type = NONE_COLL; lambda_coll = -1
         if( np.any(total_bool) ):       # there is some collision
-            total_lambda = np.concatenate( (l_self_hand, l_enemy_hand, l_self_hand_wall, l_enemy_hand_wall, l_wall_coll) )
+            total_lambda = np.concatenate( ([l_self_hand], [l_enemy_hand], l_self_hand_wall, l_enemy_hand_wall, l_wall_coll) )
             where_coll = np.where( total_bool )
             arg_min_i = np.argmin( total_lambda[total_bool] )
             which_coll = where_coll[arg_min_i]
             lambda_coll = total_lambda[which_coll]
-            wall_i = which_coll - 6
+            wall_i = int(which_coll - 12)
+            # print(ball_trajectory, total_lambda, wall_i,lambda_coll)
             if( which_coll == 0 ):      # self_hand
                 coll_type = SELF_HAND_BALL_COLLISION
             elif( which_coll == 1 ):    # enemy_hand
                 coll_type = ENEMY_HAND_BALL_COLLSION
-            elif( which_coll == 2 ):
+            elif( which_coll >= 2 and which_coll <= 5 ):
                 coll_type = SELF_HAND_WALL_COLLISION
-            elif( which_coll == 3 ):
+            elif( which_coll >= 6 and which_coll <= 9 ):
                 coll_type = ENEMY_HAND_WALL_COLLISION
-            elif( which_coll == 4 ):
+            elif( which_coll == 10 ):
                 coll_type = GOAL_TO_SELF
-            elif( which_coll == 5 ):
+            elif( which_coll == 11 ):
                 coll_type = GOAL_TO_ENEMY
             else:
                 coll_type = WALL_COLLISION
@@ -242,16 +248,18 @@ class air_hockey:
         (any_coll, coll_type, lambda_coll, wall_i ) = self.check_any_coll([prev_ball_pos, new_ball_pos], 
                 [self_prev_hand_pos, self_new_hand_pos], [enemy_prev_hand_pos, enemy_new_hand_pos])
 
+        print("coll_type = ", coll_type, "wall_i = ", wall_i)
         time_left = self.dt*(1-lambda_coll)
         while any_coll:
             if( coll_type == SELF_HAND_WALL_COLLISION ):
                 ( self_prev_hand_pos, self_new_hand_pos, self.self_hand_vel) = self.hand_wall_coll(self_prev_hand_pos, self_new_hand_pos, lambda_coll)
+                print("self", self_new_hand_pos)
                 enemy_prev_hand_pos = (1-lambda_coll)*enemy_prev_hand_pos + lambda_coll*enemy_new_hand_pos
-                prev_ball_pos = (1-lambda_coll)*prev_ball_pos + new_ball_pos
+                prev_ball_pos = (1-lambda_coll)*prev_ball_pos + lambda_coll*new_ball_pos
             elif( coll_type == ENEMY_HAND_WALL_COLLISION ):
                 ( enemy_prev_hand_pos, enemy_new_hand_pos, self.enemy_hand_vel) = self.hand_wall_coll(enemy_prev_hand_pos, enemy_new_hand_pos, lambda_coll)
                 self_prev_hand_pos = (1-lambda_coll)*self_prev_hand_pos + lambda_coll*self_new_hand_pos
-                prev_ball_pos = (1-lambda_coll)*prev_ball_pos + new_ball_pos
+                prev_ball_pos = (1-lambda_coll)*prev_ball_pos + lambda_coll*new_ball_pos
             elif( coll_type == SELF_HAND_BALL_COLLISION ):
                 ( prev_ball_pos, new_ball_pos, self_prev_hand_pos, self_new_hand_pos, self.ball_vel, 
                     self.self_hand_vel ) = self.hand_ball_coll(prev_ball_pos, new_ball_pos, self_prev_hand_pos,
@@ -290,31 +298,32 @@ class air_hockey:
             self.enemy_hand_contour = self.enemy_hand_contour - self.enemy_hand_pos + enemy_new_hand_pos
         
         self.ball_pos = new_ball_pos; self.self_hand_pos = self_new_hand_pos; self.enemy_hand_pos = enemy_new_hand_pos
-                
+        ball_drag_push = 9.8*self.m_ball*self.drag_coeff*self.dt
+        if ball_drag_push > np.linalg.norm(self.ball_vel):
+            self.ball_vel = np.zeros(2)
+        else:
+            ball_vel_norm = np.linalg.norm(self.ball_vel)
+            self.ball_vel = (ball_vel_norm - ball_drag_push)*self.ball_vel/np.max([ball_vel_norm , 1e-10])
 
+        hand_drag_push = 9.8*self.m_hand*self.drag_coeff*self.dt
+        
+        if hand_drag_push > np.linalg.norm(self.self_hand_vel):
+            self.self_hand_vel = np.zeros(2)
+        else:
+            ball_vel_norm = np.linalg.norm(self.self_hand_vel)
+            self.self_hand_vel = (ball_vel_norm - hand_drag_push)*self.self_hand_vel/np.max([ball_vel_norm , 1e-10])
 
-
+        if hand_drag_push > np.linalg.norm(self.enemy_hand_vel):
+            self.enemy_hand_vel = np.zeros(2)
+        else:
+            ball_vel_norm = np.linalg.norm(self.enemy_hand_vel)
+            self.enemy_hand_vel = (ball_vel_norm - hand_drag_push)*self.enemy_hand_vel/np.max([ball_vel_norm , 1e-10])
+        
 
     #
     # ball and physics
     #
 
-
-def update_line(i, Yhat, line):
-    line.set_data(np.array([Yhat[i,:,0],Yhat[i,:,1]]))
-    line.set_3d_properties(Yhat[i,:,2])
-    return line
-
-    
-
-
-'''
-ball_pos = np.random.rand(2)*x_width;
-ball_pos[1] = ball_pos[1]*y_heigth/x_width;
-
-ball_vel = np.random.rand(2)
-
-'''
 
 
 
